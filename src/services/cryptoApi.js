0549ef03fd4d1ff0d6d3a8d9a456e9c3f0092a80
Mladen Savic <mladenqdev@gmail.com>
2021-09-22 16:53:38 +0200

    finished cryptocurrencies page & half of homepage (news section missing)

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '34ec8264a0msh39eb8a0c8e9521ap150810jsne5e2559c6c88',
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+    }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
